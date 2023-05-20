edb0fff8bc12c3c3d65ead636eb4515db4f06b36
castroploiin <fieryfire1005@gmail.com>
2023-05-20 17:01:05 +0400

    Got it working

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,38 @@
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-struct QueryObjects {
-    synonyms: Vec<Synonym>
-}
-#[derive(Deserialize, Debug)]
 struct Synonym {
     word: String, 
     score: usize,
     tags: Vec<String>,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let word = "today";
-    let link: &str = &format!("http://api.datamuse.com/words?ml={}", word);
-    let request = reqwest::get(link).await?.text().await?;
-    let end_cutoff = &request.len() - 1;
-    let request_ref = &request[1..end_cutoff];
-    let json = serde_json::from_str::<QueryObjects>(&request_ref);
+async fn get_synonyms(of_word: &str) -> Result<Vec<Synonym>, reqwest::Error> {
+    let link: &str = &format!("http://api.datamuse.com/words?ml={}", of_word);
+    let query: Vec<Synonym> = reqwest::get(link).await?.json::<Vec<Synonym>>().await?;
+    
+    Ok(query)
+}
 
-    println!("{:#?}", request);
-    println!("{:#?}", json);
-    // match json {
-    //     Err(err) => println!("wow")
-    // }
+async fn rewrite_sentence(sentence: &str) -> Result<String, reqwest::Error> {
+    let mut replacing_words: Vec<String> = Vec::new();
+    for word in sentence.split(' ') {
+        // let synonym = &get_synonyms(word).await?[0].word;
+        replacing_words.push(get_synonyms(word).await?[0].word.to_string())
+    }    
+    
+    let rewritten: String = replacing_words.join(" ");
+    Ok(rewritten)
+}
+
+fn parse_arg_sentence() {
+
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {    
+    let s: &str = "incredibly engaging presenter";
+    println!("{}", rewrite_sentence(s).await?);
 
     Ok(())
 }
